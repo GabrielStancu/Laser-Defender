51a4b4134ecaa5c3b37f2018aefd56818fad34f6
Gabriel Stancu <gabriel.stancu07@yahoo.com>
2020-09-23 13:59:18 +0300

    project files upload

---
+++ Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour {
+
+    WaveConfig waveConfig;
+    List<Transform> waypoints;
+    int wayPointIndex = 0;
+
+    // Use this for initialization
+    private void Start()
+    {
+        waypoints = waveConfig.GetWayPoints();
+        transform.position = waypoints[wayPointIndex].transform.position;
+    }
+
+    // Update is called once per frame 
+    private void Update()
+    {
+        Move();
+    }
+
+    public void SetWaveConfig(WaveConfig waveConfig)
+    {
+        this.waveConfig = waveConfig;
+    }
+
+    private void Move()
+    {
+        if (wayPointIndex <= waypoints.Count - 1)
+        {
+            var targetPosition = waypoints[wayPointIndex].transform.position;
+            var movementThisFrame = waveConfig.GetMoveSpeed() * Time.deltaTime;
+            transform.position = Vector2.MoveTowards
+                (transform.position, targetPosition, movementThisFrame);
+
+            if (transform.position == targetPosition)
+            {
+                wayPointIndex++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/HighScoreDisplay.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+
+public class HighScoreDisplay : MonoBehaviour {
+
+    int score;
+    int highscore = 0;
+    Text highScoreText;
+
+	// Use this for initialization
+	void Start ()
+    {
+        SetHighScore();
+        DisplayHighScore();
+    }  
+
+    private void SetHighScore()
+    {
+        score = FindObjectOfType<GameSession>().GetScore();
+        highscore = PlayerPrefs.GetInt("highscore", highscore);
+        if (score > highscore)
+        {
+            highscore = score;
+            PlayerPrefs.SetInt("highscore", highscore);
+        }
+    }
+
+    private void DisplayHighScore()
+    {
+        highScoreText = GetComponent<Text>();
+        highScoreText.text = highscore.ToString();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        
+	}
+
+    
+}
